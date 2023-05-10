be7e7a459da9a3217bf969e97762c25308ce6c9e
Pablo <pablonaranjocid@gmail.com>
2023-05-10 12:54:11 +0200

    Creamos los primeros tests Pablo

---
+++ TablaEnteros2/src/tablaEnteros/TablaEnterosTest.java
@@ -0,0 +1,59 @@
+package tablaEnteros;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TablaEnterosTest {
+
+	private TablaEnteros tabla;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("INCIO DE LOS TESTS");
+		System.out.println();
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		System.out.println("FINAL DE LOS TESTS");
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		System.out.println("Inicio de la prueba");
+		Integer[] elementos = { 1, 2, 3, 4, 5};
+		tabla = new TablaEnteros(elementos);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		System.out.println("Final de la prueba");
+	}
+
+	@Test
+	void testTablaEnteros() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSumaTabla() {
+		assertEquals(15, tabla.sumaTabla());
+	}
+
+	@Test
+	void testMayorTabla() {
+		assertEquals(5, tabla.mayorTabla());
+
+	}
+
+	@Test
+	void testPosicionTabla() {
+		fail("Not yet implemented");
+	}
+
+}
